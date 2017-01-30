c93d9d0cd1c2ac4707248db75351510a48d5820e
Harry Aydin <harry.aydin.23@gmail.com>
2017-01-30 13:02:15 +1100

    Updates

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-export const DEBOUNCE_DELAY_MS = 400;
-export const SORT_OPTIONS = {
-  Title_Asc: 'Alphabetically Ascending',
-  Title_Desc: 'Alphabetically Descending',
-  Year_Asc: 'Year Ascending',
-  Year_Desc: 'Year Descending',
-  imdbRating_Asc: 'Rating Ascending',
-  imdbRating_Desc: 'Rating Descending',
-};
